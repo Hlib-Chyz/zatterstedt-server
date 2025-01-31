b647722cf861d0da48eebcfd2f79c83df065d2fa
GlebChiz <gchyz@jsdaddy.io>
2025-01-31 11:01:37 +0200

    feat: allowedHeaders

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap(): Promise<void> {
             'https://zatterstedt-admin.vercel.app',
             'https://demo-zatterstedt-admin.vercel.app',
         ],
+        allowedHeaders: '*',
     });
     app.useGlobalPipes(
         new ValidationPipe({
